ad287a51a385e7046012cb2461d4e909634b531f
42b883 <42b883@gmail.com>
2019-04-03 23:21:05 +0600

    search filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 class Main extends Component {
     state = {
-        contacts: [ ]
+        contacts: [ ],
+        search: ''
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/users')
@@ -15,10 +16,20 @@ class Main extends Component {
             })
     }
 
+    handleSearch = (e) => {
+        this.setState({
+            search: e.target.value    
+        })
+    }
+
     render() {
         const { contacts } = this.state;
-        const contactList = contacts.length ? (
-            contacts.map(contact => {
+        let filteredContacts = contacts.filter(contact => {
+                return contact.name.toLowerCase().includes(this.state.search.toLowerCase());
+            }
+        )
+        let contactList = contacts.length ? (
+            filteredContacts.map(contact => {
                 return (
                     <div className="post card" key={contact.id}>
                         <div className="card-content">
@@ -33,12 +44,30 @@ class Main extends Component {
                 )
             })
         ) : (
-            <div className="center">Contact list is empty</div>
+            <div className="center">Contacts list is empty</div>
         )
        
         return (
             <div className="container">
                 <h4 className="center">Main</h4>
+                <div className="row">
+                <div className="col s12">
+                 <div className="row">
+                    <div className="input-field col s12">
+                    <i className="material-icons prefix">search</i>
+                    <input
+                     type="text" 
+                     id="autocomplete-input"
+                     className="autocomplete"
+                     onChange={this.handleSearch}
+                     value={this.state.search}
+                     placeholder="Search for name..."
+                     />
+                    {/* <label for="autocomplete-input">search the best</label> */}
+                    </div>
+                  </div>
+                </div>
+             </div>
                 {contactList}
             </div>
         )
